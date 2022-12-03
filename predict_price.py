ce99119cfba3ad58ee3a1179bb7587919cf06693
nub69 <shabik.ashab@gmail.com>
2022-12-03 16:47:51 +0600

    use sklear for linearregression predict

---
+++ predict_price.py
@@ -0,0 +1,9 @@
+import pandas
+from sklearn.linear_model import LinearRegression
+
+data = pandas.read_csv('iphone_price.csv')
+
+model = LinearRegression()
+model.fit(data[['version']], data[['price']])
+predicted_price = model.predict([[16]])
+print(predicted_price)
